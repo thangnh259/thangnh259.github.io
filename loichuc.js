e5b619d651eac12e54efdc5761708895dc387d1f
thangnh259 <39739549+thangnh259@users.noreply.github.com>
2020-01-24 08:18:13 +0700

    Add files via upload

---
+++ loichuc.js
@@ -0,0 +1,11 @@
+﻿  var i=0,text;
+  text = "Chúc năm mới An khang thịnh vượng, tài lộc đầy nhà,một năm bình an và sung túc."
+
+  function typing() {
+    if(i<text.length){
+      document.getElementById("text").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typing,90);
+    }
+  }
+  typing();
