1aa572f4b0dc599f65d790716ddc42ab34ec4374
thangnh259 <39739549+thangnh259@users.noreply.github.com>
2020-02-06 09:20:57 +0700

    Update loichuc.js

--- loichuc.js
+++ loichuc.js
@@ -1,5 +1,5 @@
 ﻿  var i=0,text;
-  text = "Chúc năm mới An khang thịnh vượng, tài lộc đầy nhà,một năm bình an và sung túc."
+  text = "===============================================================================================."
 
   function typing() {
     if(i<text.length){
